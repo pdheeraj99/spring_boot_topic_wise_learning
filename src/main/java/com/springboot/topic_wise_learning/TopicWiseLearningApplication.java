54e020cf4b1c40bc3f492aa8eeb86b44891d90e5
dheeraj <pilladheeraj@gmail.com>
2023-11-10 20:05:05 +0530

    first commit

---
+++ src/main/java/com/springboot/topic_wise_learning/TopicWiseLearningApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.topic_wise_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicWiseLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicWiseLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/topic_wise_learning/TopicWiseLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.topic_wise_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicWiseLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
