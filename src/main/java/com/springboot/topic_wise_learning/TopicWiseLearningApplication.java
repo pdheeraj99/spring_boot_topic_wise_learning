336e9fc853da2f8925b13d987522d582abe23876
dheeraj <pilladheeraj@gmail.com>
2023-11-10 20:43:20 +0530

    Response converting to JSON under the wood

--- src/main/java/com/springboot/topic_wise_learning/TopicWiseLearningApplication.java
+++ src/main/java/com/springboot/topic_wise_learning/TopicWiseLearningApplication.java
@@ -2,12 +2,45 @@ package com.springboot.topic_wise_learning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TopicWiseLearningApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TopicWiseLearningApplication.class, args);
 	}
 
+	@GetMapping
+	Customer getCustomer() {
+		return new Customer(1L, "Dheeraj");
+	}
+
+	public class Customer {
+		private Long id;
+		private String name;
+
+		public Customer(Long id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+	}
 }
