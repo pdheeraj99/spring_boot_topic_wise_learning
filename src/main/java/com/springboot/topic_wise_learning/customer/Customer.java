56ceb4f300dca1877e89370d800f1c3650549c43
dheeraj <pilladheeraj@gmail.com>
2023-11-11 11:48:49 +0530

    When we want to ignore anything in JSON returning
    {
    "id":1,
    "name:: abc
    }
    to
    {
    "name:: abc
    }

--- src/main/java/com/springboot/topic_wise_learning/customer/Customer.java
+++ src/main/java/com/springboot/topic_wise_learning/customer/Customer.java
@@ -1,5 +1,7 @@
 package com.springboot.topic_wise_learning.customer;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public class Customer {
    private final Long id;
    private final String name;
@@ -11,6 +13,7 @@ public class Customer {
 
    // Jakson library is returning JSON format based on this getters only
 
+   @JsonIgnore
    public Long getId() {
       return id;
    }
